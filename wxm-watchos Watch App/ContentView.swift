2ac99806eddecdfa592c190428efc539ce266d2e
mgarciate <mgarciate@gmail.com>
2024-05-19 11:52:57 +0200

    Add watchOS target

---
+++ wxm-watchos Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  wxm-watchos Watch App
+//
+//  Created by mgarciate on 18/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ wxm-watchos Watch App/wxm_watchosApp.swift
@@ -0,0 +1,17 @@
+//
+//  wxm_watchosApp.swift
+//  wxm-watchos Watch App
+//
+//  Created by mgarciate on 18/5/24.
+//
+
+import SwiftUI
+
+@main
+struct wxm_watchos_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
