3b2b9aabe0b3efa384263737f91b1d18b5e4a069
mgarciate <mgarciate@gmail.com>
2024-05-21 23:51:53 +0200

    WIP Style Device Card View

--- wxm-watchos Watch App/WeatherStationsHomeView.swift
+++ wxm-watchos Watch App/WeatherStationsHomeView.swift
@@ -14,19 +14,54 @@ struct WeatherStationsHomeView: View {
                 NavigationLink {
                     StationDetailsContainerView(index: index)
                 } label: {
-                    VStack {
+                    VStack(alignment: .leading) {
                         Text("3 letters name")
-                        HStack(spacing: CGFloat(8.0)) {
-                            Text(FontIcon.hexagon.rawValue)
-                                .font(.fontAwesome(font: .FAPro, size: 10.0))
-                                .foregroundColor(Color.white)
+                        HStack {
+//                            HStack(spacing: 0.0) {
+//                                Image(asset: .wifi)
+//                                    .renderingMode(.template)
+//                                    .foregroundColor(Color(colorEnum: configuration.stateColor))
+//
+//                                Text(configuration.lastActiveAt?.lastActiveTime() ?? "-")
+//                                    .font(.system(size: CGFloat(.caption)))
+//                                    .foregroundColor(Color(colorEnum: configuration.stateColor))
+//                                    .padding(.trailing, CGFloat(.smallSidePadding))
+//                            }
+//                            .WXMCardStyle(backgroundColor: Color(colorEnum: configuration.tintColor),
+//                                          insideHorizontalPadding: 0.0,
+//                                          insideVerticalPadding: 0.0,
+//                                          cornerRadius: CGFloat(.buttonCornerRadius))
+                            HStack(spacing: 0.0) {
+                                Image(asset: .wifi)
+                                    .renderingMode(.template)
+                                    .foregroundColor(Color.blue)
 
-                            Text("address")
-                                .font(.system(size: CGFloat(10.0)))
-                                .foregroundColor(Color.white)
-                                .lineLimit(1)
+                                Text("lastActiveAt")
+                                    .font(.system(size: CGFloat(10.0)))
+                                    .foregroundColor(Color.green)
+                                    .padding(.trailing, CGFloat(4))
+                            }
+                            .WXMCardStyle(backgroundColor: Color.pink,
+                                          insideHorizontalPadding: 0.0,
+                                          insideVerticalPadding: 0.0,
+                                          cornerRadius: CGFloat(4))
+                            HStack(spacing: CGFloat(8.0)) {
+                                Text(FontIcon.hexagon.rawValue)
+                                    .font(.fontAwesome(font: .FAPro, size: 10.0))
+                                    .foregroundColor(Color(colorEnum: .text))
+
+                                Text("address")
+                                    .font(.system(size: CGFloat(10.0)))
+                                    .foregroundColor(Color(colorEnum: .text))
+                                    .lineLimit(1)
+                            }
+                            .WXMCardStyle(backgroundColor: Color(colorEnum: .blueTint),
+                                          insideHorizontalPadding: CGFloat(8),
+                                          insideVerticalPadding: CGFloat(4),
+                                      cornerRadius: CGFloat(4))
                         }
                     }
+                    .background(Color.green)
                 }
             }
             .navigationTitle("Select a Station")
